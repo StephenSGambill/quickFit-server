5766d7a90accd5d5646c43e79ece41a0e3c5f895
Stephen Gambill <stephensgambill@gmail.com>
2023-06-07 13:12:29 -0500

    slight tweak with Exercise Model

--- qfsapi/models/exercise.py
+++ qfsapi/models/exercise.py
@@ -6,3 +6,6 @@ class Exercise(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=40)
     gif = models.URLField("https://giphy.com/clips/theoffice-3rS8HpFMbQzQ7p925V")
+    duration = models.IntegerField(default=30, max_length=10)
+    rest = models.IntegerField(default=10, max_length=10)
+    iterations = models.IntegerField(default=3, max_length=10)
