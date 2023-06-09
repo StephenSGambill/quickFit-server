6fb83f5d8ee9e0c9c3264c4f2ab426bba280b5a2
Stephen Gambill <stephensgambill@gmail.com>
2023-06-09 10:59:15 -0500

    completed ability to create new exercise, delete, and modify existing exercise

--- qfsapi/serializers/__init__.py
+++ qfsapi/serializers/__init__.py
@@ -1,4 +1,4 @@
 from .member_serializer import MemberSerializer, MemberUserSerializer
 from .workout_serializer import WorkoutSerializer, CompletedWorkoutSerializer
-from .exercise_serializer import ExerciseSerializer
+from .exercise_serializer import ExerciseSerializer, NewExerciseSerializer
 from .custom_workout_serializer import CustomWorkoutSerializer
