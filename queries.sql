ed0e3904e3b5ea800deafa2d5925c518afaff53d
Stephen Gambill <stephensgambill@gmail.com>
2023-06-30 14:53:21 -0500

    created fixtures for re-loading

--- queries.sql
+++ queries.sql
@@ -4,3 +4,15 @@ WHERE id =2
 UPDATE auth_user
 SET is_staff = True
 WHERE id = 3
+
+
+DELETE FROM qfsapi_workoutexercise;
+DELETE FROM qfsapi_workout;
+DELETE FROM qfsapi_exercise;
+
+
+UPDATE qfsapi_completedworkout
+SET member_id = 1
+WHERE id = 3
+
+
